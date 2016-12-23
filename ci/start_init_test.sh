9824fafa7914becf463bda77d932a2142083b59e
payes <payes.anand@cloudbyte.com>
2016-12-23 16:18:50 +0530

    Update start_init_test.sh

--- ci/start_init_test.sh
+++ ci/start_init_test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 sudo docker network create --subnet=172.18.0.0/16 longhorn-net
-sudo docker run -d -it --net longhorn-net --ip 172.18.0.3 --expose 9502-9504  -v vol1:/volume  -v /mnt/vol1:/vol1  $1 launch replica --listen 172.18.0.3:9502 --size 10g /vol1
-sudo docker run -d --net longhorn-net --ip 172.18.0.2 -P  --expose 3260 --expose 9502-9504  $1 launch controller --frontend gotgt --replica tcp://172.18.0.3:9502 store1 172.18.0.2
-sudo docker run -d -it --net longhorn-net --ip 172.18.0.4 --expose 9502-9504 -v vol2:/volume -v /mnt/vol2:/vol2 $1 launch replica --listen 172.18.0.4:9502 --size 10g /vol1
-sudo docker ps | grep controller | awk '{print "sudo docker exec -it "$1" longhorn --debug add-replica tcp://172.18.0.4:9502"}' > start_test1.sh
-sh start_test1.sh
+sudo docker run -d -it --net longhorn-net --ip 172.18.0.3 --expose 9502-9504 -v /mnt/vol1:/vol1  $1 launch replica --frontendIP 172.18.0.2 --listen 172.18.0.3:9502 --size 10g /vol1
+sudo docker run -d --net longhorn-net --ip 172.18.0.2 -P --expose 3260 --expose 9501  $1 launch controller --frontend gotgt --frontendIP 172.18.0.2 --replica tcp://172.18.0.3:9502 store1
+sudo docker run -d -it --net longhorn-net --ip 172.18.0.4 --expose 9502-9504 -v /mnt/vol2:/vol2 $1 launch replica --frontendIP 172.18.0.2 --listen 172.18.0.4:9502 --size 10g /vol1
+
 sudo umount /mnt/store
 sudo iscsiadm -m node -u
 sudo iscsiadm -m node -o delete
