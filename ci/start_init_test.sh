9445105865f7bfa4c46235b24837d0683788177d
ksatchit <karthik.s@cloudbyte.com>
2018-03-12 20:29:58 +0530

    Compliance test success/failure condition
    Signed-off-by: ksatchit <karthik.s@cloudbyte.com>

--- ci/start_init_test.sh
+++ ci/start_init_test.sh
@@ -72,7 +72,13 @@ if [ "$x"!="" ]; then
         # TEST#3: Run the libiscsi compliance suite on Jiva Vol
         sudo mkdir /mnt/logs 
         sudo docker run -v /mnt/logs:/mnt/logs --net host ksatchit/libiscsi /bin/bash -c "./testiscsi.sh --ctrl-svc-ip 172.18.0.2"
-
+        tp=$(grep "PASSED" $(find /mnt/logs -name SUMMARY.log) | wc -l)
+        tf=$(grep "FAILED" $(find /mnt/logs -name SUMMARY.log) | wc -l)
+        if [ $tp -ge 146 ] && [ $tf -le 29 ]; then
+            echo "iSCSI Compliance test: PASSED"
+        else 
+            echo "iSCSI Compliance test: FAILED"; exit 1
+        fi
 else 
         echo "Unable to detect iSCSI device, login failed"; exit 1
 fi 
