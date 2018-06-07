9af778b482e681f211eef100a1a940841fb33368
Payes <payes.anand@cloudbyte.com>
2018-06-07 23:07:34 +0530

    Add description of the test cases in the ci script
    Signed-off-by: Payes <payes.anand@cloudbyte.com>

--- ci/start_init_test.sh
+++ ci/start_init_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#SIGUSR1 increases the delay by 2 seconds at certain places in controller/replica
+#SIGUSR2 decreases the delay by 2 seconds at certain places in controller/replica
+
 CONTROLLER_IP="172.18.0.2"
 REPLICA_IP1="172.18.0.3"
 REPLICA_IP2="172.18.0.4"
@@ -8,13 +11,13 @@ CLONED_CONTROLLER_IP="172.18.0.6"
 CLONED_REPLICA_IP="172.18.0.7"
 
 prepare_test_env() {
-	sudo rm -rf /tmp/vol*
-	sudo rm -rf /mnt/logs
-	sudo mkdir -p /tmp/vol1 /tmp/vol2 /tmp/vol3 /tmp/vol4
-	sudo mkdir -p /mnt/store /mnt/store2
+	rm -rf /tmp/vol*
+	rm -rf /mnt/logs
+	mkdir -p /tmp/vol1 /tmp/vol2 /tmp/vol3 /tmp/vol4
+	mkdir -p /mnt/store /mnt/store2
 
-	sudo docker network create --subnet=172.18.0.0/16 stg-net
-	JI=$(sudo docker images | grep openebs/jiva | awk '{print $1":"$2}')
+	docker network create --subnet=172.18.0.0/16 stg-net
+	JI=$(docker images | grep openebs/jiva | awk '{print $1":"$2}')
 	echo "Run CI tests on $JI"
 }
 
@@ -41,21 +44,21 @@ verify_rw_status() {
 
 # start_controller CONTROLLER_IP
 start_controller() {
-	controller_id=$(sudo docker run -d --net stg-net --ip "$1" -P --expose 3260 --expose 9501 --expose 9502-9504 $JI \
+	controller_id=$(docker run -d --net stg-net --ip "$1" -P --expose 3260 --expose 9501 --expose 9502-9504 $JI \
 		launch controller --frontend gotgt --frontendIP "$1" "$2")
 	echo "$controller_id"
 }
 
 # start_replica CONTROLLER_IP REPLICA_IP folder_name
 start_replica() {
-	replica_id=$(sudo docker run -d -it --net stg-net --ip "$2" -P --expose 9502-9504 -v /tmp/"$3":/"$3" $JI \
+	replica_id=$(docker run -d -it --net stg-net --ip "$2" -P --expose 9502-9504 -v /tmp/"$3":/"$3" $JI \
 		launch replica --frontendIP "$1" --listen "$2":9502 --size 2g /"$3")
 	echo "$replica_id"
 }
 
 # start_cloned_replica CONTROLLER_IP  CLONED_CONTROLLER_IP CLONED_REPLICA_IP folder_name
 start_cloned_replica() {
-	cloned_replica_id=$(sudo docker run -d -it --net stg-net --ip "$3" -P --expose 9502-9504 -v /tmp/"$4":/"$4" $JI \
+	cloned_replica_id=$(docker run -d -it --net stg-net --ip "$3" -P --expose 9502-9504 -v /tmp/"$4":/"$4" $JI \
 		launch replica --type clone --snapName snap1 --cloneIP "$1" --frontendIP "$2" --listen "$3":9502 --size 2g /"$4")
 	echo "$cloned_replica_id"
 }
@@ -67,9 +70,9 @@ get_replica_count() {
 }
 
 test_single_replica_stop_start() {
-	sudo docker stop $replica1_id
+	docker stop $replica1_id
 	sleep 5
-	sudo docker start $replica1_id
+	docker start $replica1_id
 	if [ $(verify_rw_status "RW") == "0" ]; then
 		echo "Single replica stop/start test passed"
 	else
@@ -79,7 +82,7 @@ test_single_replica_stop_start() {
 }
 
 test_two_replica_stop_start() {
-	sudo docker stop $replica1_id
+	docker stop $replica1_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 2 replicas and one is stopped"
 	else
@@ -87,7 +90,7 @@ test_two_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker start $replica1_id
+	docker start $replica1_id
 	if [ $(verify_rw_status "RW") == 0 ]; then
 		echo "stop/start test passed when there are 2 replicas and one is restarted"
 	else
@@ -95,8 +98,8 @@ test_two_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker stop $replica1_id
-	sudo docker stop $replica2_id
+	docker stop $replica1_id
+	docker stop $replica2_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 2 replicas and both are stopped"
 	else
@@ -104,7 +107,7 @@ test_two_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker start $replica1_id
+	docker start $replica1_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 2 replicas and one is restarted"
 	else
@@ -112,7 +115,7 @@ test_two_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker start $replica2_id
+	docker start $replica2_id
 	if [ $(verify_rw_status "RW") == 0 ]; then
 		echo "Dual replica stop/start test passed"
 	else
@@ -126,7 +129,11 @@ run_ios_to_test_stop_start() {
 	sleep 5
 	get_scsi_disk
 	if [ "$device_name"!="" ]; then
-		sudo dd if=/dev/urandom of=/dev/$device_name bs=4k count=1000
+		dd if=/dev/urandom of=/dev/$device_name bs=4k count=1000
+		if [ $? -eq 0 ]; then echo "IOs were written successfully while running 3 replicas stop/start test"
+		else
+			echo "IOs errored out while running 3 replicas stop/start test"; exit 1
+		fi
 		logout_of_volume
 		sleep 5
 	else
@@ -135,18 +142,21 @@ run_ios_to_test_stop_start() {
 }
 
 test_three_replica_stop_start() {
-	sudo docker kill --signal=SIGUSR1 $replica1_id
-	sudo docker kill --signal=SIGUSR1 $replica1_id
+	# Add 4 sec delay in serving IOs from replica1, start IOs, and then close replica1
+	# This will trigger the quorum condition which checks if the IOs are 
+	# written to more than 50% of the replicas
+	docker kill --signal=SIGUSR1 $replica1_id
+	docker kill --signal=SIGUSR1 $replica1_id
 
 	run_ios_to_test_stop_start &
-	sudo docker stop $replica1_id
+	docker stop $replica1_id
 	if [ $(verify_rw_status "RW") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and one is stopped"
 	else
 		echo "stop/start test failed when there are 3 replicas and one is stopped"
 		exit 1
 	fi
-	sudo docker stop $replica2_id
+	docker stop $replica2_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and two are stopped"
 	else
@@ -154,7 +164,7 @@ test_three_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker stop $replica3_id
+	docker stop $replica3_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and all are stopped"
 	else
@@ -162,18 +172,22 @@ test_three_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker start $replica1_id
+	docker start $replica1_id
 	if [ $(verify_rw_status "RO") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and one is restarted"
 	else
 		echo "stop/start test failed when there are 3 replicas and one is restarted"
 		exit 1
 	fi
+	# Sending SIGUSR1 to controller twice adds delay of 4 seconds in the controller code at some places
+	# It has been introduced just before starting 2nd and 3rd replica.
+	# Path being tested here is the race condition which should be hit as both the replicas 
+	# are added at the same time at the controller. So that both the replicas are opened but 
+	# only one is attached successfully and the other is left in open state.
+	docker kill --signal=SIGUSR1 $controller_id
+	docker kill --signal=SIGUSR1 $controller_id
 
-	sudo docker kill --signal=SIGUSR1 $controller_id
-	sudo docker kill --signal=SIGUSR1 $controller_id
-
-	sudo docker start $replica2_id
+	docker start $replica2_id
 	if [ $(verify_rw_status "RW") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and two are restarted"
 	else
@@ -181,7 +195,7 @@ test_three_replica_stop_start() {
 		exit 1
 	fi
 
-	sudo docker start $replica3_id
+	docker start $replica3_id
 	if [ $(verify_rw_status "RW") == 0 ]; then
 		echo "stop/start test passed when there are 3 replicas and all are restarted"
 	else
@@ -238,15 +252,15 @@ run_vdbench_test_on_volume() {
 	sleep 5
 	get_scsi_disk
 	if [ "$device_name"!="" ]; then
-		sudo mount /dev/$device_name /mnt/store
-		sudo mkdir -p /mnt/store/data
-		sudo chown 777 /mnt/store/data
-		sudo docker run -v /mnt/store/data:/datadir1 openebs/tests-vdbench:latest
+		mount /dev/$device_name /mnt/store
+		mkdir -p /mnt/store/data
+		chown 777 /mnt/store/data
+		docker run -v /mnt/store/data:/datadir1 openebs/tests-vdbench:latest
 		if [ $? -eq 0 ]; then echo "VDbench Test: PASSED"
 		else
 			echo "VDbench Test: FAILED";exit 1
 		fi
-		sudo umount /mnt/store
+		umount /mnt/store
 	else
 		echo "Unable to detect iSCSI device, login failed"; exit 1
 	fi
@@ -255,8 +269,8 @@ run_vdbench_test_on_volume() {
 
 run_libiscsi_test_suite() {
 	echo "Run the libiscsi compliance suite on Jiva Vol"
-	sudo mkdir /mnt/logs
-	sudo docker run -v /mnt/logs:/mnt/logs --net host openebs/tests-libiscsi /bin/bash -c "./testiscsi.sh --ctrl-svc-ip $CONTROLLER_IP"
+	mkdir /mnt/logs
+	docker run -v /mnt/logs:/mnt/logs --net host openebs/tests-libiscsi /bin/bash -c "./testiscsi.sh --ctrl-svc-ip $CONTROLLER_IP"
 	tp=$(grep "PASSED" $(find /mnt/logs -name SUMMARY.log) | wc -l)
 	tf=$(grep "FAILED" $(find /mnt/logs -name SUMMARY.log) | wc -l)
 	if [ $tp -ge 146 ] && [ $tf -le 29 ]; then
@@ -267,14 +281,14 @@ run_libiscsi_test_suite() {
 }
 
 logout_of_volume() {
-	sudo iscsiadm -m node -u
-	sudo iscsiadm -m node -o delete
+	iscsiadm -m node -u
+	iscsiadm -m node -o delete
 }
 
 # Discover Jiva iSCSI target and Login
 login_to_volume() {
-	sudo iscsiadm -m discovery -t st -p $1
-	sudo iscsiadm -m node -l
+	iscsiadm -m discovery -t st -p $1
+	iscsiadm -m node -l
 }
 
 # Wait for iSCSI device node (scsi device) to be created
@@ -294,28 +308,28 @@ get_scsi_disk() {
 	done
 }
 
-run_data_integrity_test(){
+run_data_integrity_test() {
 	login_to_volume "$CONTROLLER_IP:3260"
 	sleep 5
 	get_scsi_disk
 	if [ "$device_name"!="" ]; then
-		sudo mkfs.ext2 -F /dev/$device_name
+		mkfs.ext2 -F /dev/$device_name
 
-		sudo mount /dev/$device_name /mnt/store
+		mount /dev/$device_name /mnt/store
 
-		sudo dd if=/dev/urandom of=file1 bs=4k count=10000
-		hash1=$(sudo md5sum file1 | awk '{print $1}')
-		sudo cp file1 /mnt/store
-		hash2=$(sudo md5sum /mnt/store/file1 | awk '{print $1}')
+		dd if=/dev/urandom of=file1 bs=4k count=10000
+		hash1=$(md5sum file1 | awk '{print $1}')
+		cp file1 /mnt/store
+		hash2=$(md5sum /mnt/store/file1 | awk '{print $1}')
 		if [ $hash1 == $hash2 ]; then echo "DI Test: PASSED"
 		else
 			echo "DI Test: FAILED"; exit 1
 		fi
 
 		cd /mnt/store; sync; sleep 5; sync; sleep 5; cd ~;
-		sudo blockdev --flushbufs /dev/$device_name
-		sudo hdparm -F /dev/$device_name
-		sudo umount /mnt/store
+		blockdev --flushbufs /dev/$device_name
+		hdparm -F /dev/$device_name
+		umount /mnt/store
 		logout_of_volume
 		sleep 5
 	else
@@ -343,15 +357,15 @@ test_clone_feature() {
 	get_scsi_disk
 
 	if [ "$device_name"!="" ]; then
-		sudo mount /dev/$device_name /mnt/store2
+		mount /dev/$device_name /mnt/store2
 
-		hash3=$(sudo md5sum /mnt/store2/file1 | awk '{print $1}')
+		hash3=$(md5sum /mnt/store2/file1 | awk '{print $1}')
 		if [ $hash1 == $hash3 ]; then
-			sudo umount /mnt/store2
+			umount /mnt/store2
 			logout_of_volume
 			echo "DI Test: PASSED"
 		else
-			sudo umount /mnt/store2
+			umount /mnt/store2
 			logout_of_volume
 			echo "DI Test: FAILED"; exit 1
 		fi
