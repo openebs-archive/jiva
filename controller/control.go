d4bfd8f1137f37fe9ff8e6e0d2331362524debd7
Payes <payes.anand@cloudbyte.com>
2018-06-27 13:43:49 +0530

    Resolve golint error
    Signed-off-by: Payes <payes.anand@cloudbyte.com>

--- controller/control.go
+++ controller/control.go
@@ -652,9 +652,12 @@ func (c *Controller) Start(addresses ...string) error {
 	return nil
 }
 
-//Delaying error response by 1 second when volume is in read only state
-//This will avoid the iscsi disk at client side to go in read only mode even when IOs are not being served.
-//Above approach can hold the the app only for small amount of time based on the app.
+// WriteAt is the interface which can be used to write data to jiva volumes
+// Delaying error response by 1 second when volume is in read only state, this will avoid
+// avoid the iscsi disk at client side to go in read only mode even when IOs
+// are not being served.
+// Above approach can hold the the app only for small amount of time based
+// on the app.
 func (c *Controller) WriteAt(b []byte, off int64) (int, error) {
 	c.RLock()
 	if c.ReadOnly == true {
