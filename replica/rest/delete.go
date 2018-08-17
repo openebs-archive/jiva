92cd1a76e1b5b559f946b15be705d63a55c5c198
Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>
2018-08-17 13:18:40 +0530

    Add missing file and fix ci
    Signed-off-by: Utkarsh Mani Tripathi <utkarshmani1997@gmail.com>

---
+++ replica/rest/delete.go
@@ -0,0 +1,27 @@
+package rest
+
+import (
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/rancher/go-rancher/api"
+	"github.com/rancher/go-rancher/client"
+)
+
+// DeleteVolume delete all the contents of the volume. It purges all the replica
+// files.
+func (s *Server) DeleteVolume(rw http.ResponseWriter, req *http.Request) error {
+	logrus.Infof("DeleteVolume")
+	apiContext := api.GetApiContext(req)
+	err := s.s.DeleteAll()
+	if err != nil {
+		logrus.Errorf("Error %v in doOp: %v", err, req.RequestURI)
+		return err
+	}
+	apiContext.Write(&DeleteReplicaOutput{
+		client.Resource{
+			Type: "delete",
+		},
+	})
+	return nil
+}
