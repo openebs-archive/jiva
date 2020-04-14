97ed722d409645e6fd18378d250ae4dde605ef9e
Payes <payes.anand@mayadata.io>
2020-04-14 07:51:24 +0530

    Fix go vet issues
    Signed-off-by: Payes <payes.anand@mayadata.io>

--- replica/rest/model.go
+++ replica/rest/model.go
@@ -43,7 +43,7 @@ type RebuildingInput struct {
 type SnapshotInput struct {
 	client.Resource
 	Name        string `json:"name"`
-	NewHead     string `json: "newhead"`
+	NewHead     string `json:"newhead"`
 	UserCreated bool   `json:"usercreated"`
 	Created     string `json:"created"`
 }
