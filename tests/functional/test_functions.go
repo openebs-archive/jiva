9aba5e900d0ad363438cdf6332a21b3680ea93c8
Payes Anand <payes.anand@mayadata.io>
2020-07-29 18:55:40 +0530

    fix golint errors
    Signed-off-by: Payes Anand <payes.anand@mayadata.io>

--- tests/functional/test_functions.go
+++ tests/functional/test_functions.go
@@ -10,7 +10,7 @@ import (
 	"github.com/openebs/jiva/sync"
 	"github.com/openebs/jiva/util"
 	"github.com/sirupsen/logrus"
-	. "gopkg.in/check.v1"
+	checkV1 "gopkg.in/check.v1"
 )
 
 func testFunctions() {
@@ -19,89 +19,89 @@ func testFunctions() {
 
 func getDeleteCandidateChainFuncTest() {
 
-	var c *C
+	var c *checkV1.C
 	dir, err := ioutil.TempDir("", "replica")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	defer os.RemoveAll(dir)
 
 	r, err := replica.New(true, 10*4096, 4096, dir, nil, "Backend")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	defer r.Close()
 	err = r.SetReplicaMode("RW")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	now := getNow()
 	err = r.Snapshot("000", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	err = r.Snapshot("001", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	//Chain len equals 3 test
 	list, err := sync.GetDeleteCandidateChain(r, "volume-snap-001.img")
-	c.Assert(err, IsNil)
-	c.Assert(list, IsNil)
+	c.Assert(err, checkV1.IsNil)
+	c.Assert(list, checkV1.IsNil)
 
 	//Empty checkpoint test
 	list, err = sync.GetDeleteCandidateChain(r, "")
-	c.Assert(err, IsNil)
-	c.Assert(list, IsNil)
+	c.Assert(err, checkV1.IsNil)
+	c.Assert(list, checkV1.IsNil)
 
 	//Non existent checkpoint test
 	list, err = sync.GetDeleteCandidateChain(r, "fake")
-	c.Assert(err, IsNil)
-	c.Assert(list, IsNil)
+	c.Assert(err, checkV1.IsNil)
+	c.Assert(list, checkV1.IsNil)
 
 	err = r.Snapshot("002", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	// Simple test
 	list, err = sync.GetDeleteCandidateChain(r, "volume-snap-002.img")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	verifyList(list, []string{"volume-snap-001.img"})
 
 	err = r.Snapshot("003", true, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	err = r.Snapshot("004", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	err = r.Snapshot("005", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	// User Created snapshot test
 	list, err = sync.GetDeleteCandidateChain(r, "volume-snap-005.img")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	verifyList(list, []string{"volume-snap-001.img", "volume-snap-002.img", "volume-snap-004.img"})
 
 	_, err = r.PrepareRemoveDisk("volume-snap-003.img")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	// User Created snapshot removed test
 	list, err = sync.GetDeleteCandidateChain(r, "volume-snap-005.img")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	verifyList(list, []string{"volume-snap-001.img", "volume-snap-002.img", "volume-snap-003.img", "volume-snap-004.img"})
 
 	buf := make([]byte, 4096)
 	fill(buf, 1)
 	_, err = r.WriteAt(buf, 0)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	err = r.Snapshot("006", false, now) // Contains 100 bytes data
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	err = r.Snapshot("007", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	buf = make([]byte, 8192)
 	fill(buf, 2)
 	_, err = r.WriteAt(buf, 5)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	err = r.Snapshot("008", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	err = r.Snapshot("009", false, now)
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 
 	// Verify sorted chain
 	list, err = sync.GetDeleteCandidateChain(r, "volume-snap-009.img")
-	c.Assert(err, IsNil)
+	c.Assert(err, checkV1.IsNil)
 	verifyList(list, []string{"volume-snap-001.img", "volume-snap-002.img", "volume-snap-003.img", "volume-snap-004.img",
 		"volume-snap-005.img", "volume-snap-007.img", "volume-snap-006.img", "volume-snap-008.img"})
 }
