9dad8d0893bc1d8e19d4eb429685fea8e10989f1
Payes Anand <payes.anand@mayadata.io>
2020-08-12 14:30:26 +0530

    fix(auto-delete-snapshot): remove empty elements from DeleteCandidateChain (#322)
    * Fix issue with deletion of user created snapshots
    * update and improve functional tests
    
    Signed-off-by: payes <payes.anand@mayadata.io>

--- tests/e2e/ios.go
+++ tests/e2e/ios.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"github.com/sirupsen/logrus"
 	"math/rand"
 	"os"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -15,10 +17,13 @@ const (
 	gb = 1024 * mb
 )
 
-func verifyData(fd int, tid, iter, offset int64) {
+func (config *testConfig) verifyData(fd int, tid, iter, offset int64) {
 	readBuf := make([]byte, 1024)
 	prevWriteBuf := make([]byte, 1024)
 	for {
+		if config.Stop {
+			return
+		}
 		_, err := syscall.Pread(fd, readBuf, offset)
 		if err == nil {
 			break
@@ -27,13 +32,19 @@ func verifyData(fd int, tid, iter, offset int64) {
 	}
 	copy(prevWriteBuf, []byte(strconv.FormatInt(offset*tid*(iter-1), 10)))
 	if !bytes.Equal(readBuf, prevWriteBuf) {
-		panic("Data Integrity check failed")
+		if config.Stop {
+			return
+		}
+		logrus.Fatalf("Data Integrity check failed")
 	}
 }
 
-func writeData(fd int, tid, iter, offset int64) {
+func (config *testConfig) writeData(fd int, tid, iter, offset int64) {
 	writeBuf := []byte(strconv.FormatInt(offset*tid*iter, 10))
 	for {
+		if config.Stop {
+			return
+		}
 		_, err := syscall.Pwrite(fd, writeBuf, offset)
 		if err == nil {
 			break
@@ -48,13 +59,13 @@ func (config *testConfig) readVerifyWriteTestForIter(fd int, tid int64, region [
 			return
 		}
 		if iter != 1 {
-			verifyData(fd, tid, iter, offset)
+			config.verifyData(fd, tid, iter, offset)
 		}
-		writeData(fd, tid, iter, offset)
+		config.writeData(fd, tid, iter, offset)
 	}
 }
 
-func (config *testConfig) startIOs(tid int64, devPath string) {
+func (config *testConfig) startIOs(tid, iter int64, devPath string) {
 	config.insertThread()
 	defer config.releaseThread()
 	var (
@@ -62,30 +73,52 @@ func (config *testConfig) startIOs(tid int64, devPath string) {
 		fd  int
 	)
 
-	if fd, err = syscall.Open(devPath, os.O_RDWR, 0777); err != nil {
-		panic(err)
-	}
-
 	region := []int64{(tid - 1) * gb, tid * gb}
-	iter := int64(1)
-	for {
-		config.readVerifyWriteTestForIter(fd, tid, region, iter)
-		if config.Stop {
-			return
-		}
-		time.Sleep(1 * time.Second)
-		iter++
+	logrus.Infof("IO Iteration: %v for %v, tid: %v", iter, devPath, tid)
+	if fd, err = syscall.Open(devPath, os.O_RDWR, 0777); err != nil {
+		logrus.Fatalf("%v", err)
+	}
+	config.readVerifyWriteTestForIter(fd, tid, region, iter)
+	if config.Stop {
+		return
+	}
+	if err = syscall.Close(fd); err != nil {
+		logrus.Fatalf("%v", err)
 	}
 }
 
 func (config *testConfig) runIOs() {
-	devPath, err := config.attachDisk()
-	if err != nil {
-		panic(err)
-	}
-	for i := 1; i <= 5; i++ {
-		tid := i
-		go config.startIOs(int64(tid), devPath)
+	config.insertThread()
+	defer config.releaseThread()
+
+	wg := sync.WaitGroup{}
+	iter := int64(1)
+	for {
+		if config.Stop {
+			return
+		}
+		devPath, err := config.attachDisk()
+		if err != nil {
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		time.Sleep(5 * time.Second) // It takes some time for iscsi device to appear
+		wg.Add(5)
+		for i := 1; i <= 5; i++ {
+			tid := i
+			go func(tid int, iter int64) {
+				config.startIOs(int64(tid), iter, devPath)
+				wg.Done()
+			}(tid, iter)
+		}
+		wg.Wait()
+		for {
+			if err = config.detachDisk(); err == nil {
+				break
+			}
+			time.Sleep(2 * time.Second)
+		}
+		iter++
 	}
 }
 
@@ -112,12 +145,12 @@ func (config *testConfig) writeRandomData() []int64 {
 	table := generateRandomIOTable()
 	devPath, err := config.attachDisk()
 	if err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 	fillBlocks(devPath, table)
 	err = config.detachDisk()
 	if err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 	return table
 }
@@ -127,7 +160,7 @@ func fillBlocks(devPath string, table []int64) {
 		err error
 	)
 	if fd, err = syscall.Open(devPath, os.O_RDWR, 0777); err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 	for offset, data := range table {
 		writeBuf := []byte(strconv.FormatInt(data, 10))
@@ -140,19 +173,19 @@ func fillBlocks(devPath string, table []int64) {
 		}
 	}
 	if err = syscall.Close(fd); err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 }
 
 func (config *testConfig) verifyRandomData(table []int64) {
 	devPath, err := config.attachDisk()
 	if err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 	verifyBlocks(devPath, table)
 	err = config.detachDisk()
 	if err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 }
 func verifyBlocks(devPath string, table []int64) {
@@ -161,7 +194,7 @@ func verifyBlocks(devPath string, table []int64) {
 		err error
 	)
 	if fd, err = syscall.Open(devPath, os.O_RDWR, 0777); err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 	for offset, data := range table {
 		//Skip checking empty blocks
@@ -179,10 +212,10 @@ func verifyBlocks(devPath string, table []int64) {
 		}
 		copy(prevWriteBuf, []byte(strconv.FormatInt(data, 10)))
 		if !bytes.Equal(readBuf, prevWriteBuf) {
-			panic("Data Integrity check failed")
+			logrus.Fatalf("Data Integrity check failed")
 		}
 	}
 	if err = syscall.Close(fd); err != nil {
-		panic(err)
+		logrus.Fatalf("%v", err)
 	}
 }
