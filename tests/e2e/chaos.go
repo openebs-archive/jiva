04310b5e83fc2ddecbe2cc0eaafdeceda5e89eda
Payes Anand <payes.anand@mayadata.io>
2020-08-12 14:30:26 +0530

    fix(auto-delete-snapshot): remove empty elements from DeleteCandidateChain (#322)
    * Fix issue with deletion of user created snapshots
    * update and improve functional tests
    
    Signed-off-by: Payes Anand <payes.anand@mayadata.io>

--- tests/e2e/chaos.go
+++ tests/e2e/chaos.go
@@ -7,11 +7,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var TestRunTime time.Duration = 60
+
 func restartOneReplicaTest() {
 	config := buildConfig("172.18.0.10", []string{"172.18.0.11", "172.18.0.12", "172.18.0.13"})
 	setupTest(config)
 	config.verifyRWReplicaCount(3)
-	config.runIOs()
+	go config.testSequentialData()
 	go config.snapshotCreateDelete()
 	ctrlClient := getControllerClient(config.ControllerIP)
 	startTime := time.Now()
@@ -25,18 +27,23 @@ func restartOneReplicaTest() {
 		stopContainer(config.Replicas[striped(replicas[0].Address)])
 		startContainer(config.Replicas[striped(replicas[0].Address)])
 		config.verifyRWReplicaCount(3)
-		time.Sleep(30 * time.Second)
-		if time.Since(startTime) > time.Minute*20 {
+		time.Sleep(65 * time.Second) // 65 so that auto-snapshot delete is triggered
+		if time.Since(startTime) > time.Minute*TestRunTime {
 			break
 		}
 		iteration++
 	}
 	config.Stop = true
-	for {
+	for range time.NewTicker(5 * time.Second).C {
 		if config.ThreadCount == 0 {
 			break
 		}
+		logrus.Infof(
+			"RestartControllerTest Completed, waiting for threads to exit, pending tc:%v",
+			config.ThreadCount,
+		)
 	}
+	logrus.Infof("Exiting RestartOneReplicaTest, threads exited successfully")
 	scrap(config)
 }
 
@@ -44,7 +51,7 @@ func restartTwoReplicasTest() {
 	config := buildConfig("172.18.0.20", []string{"172.18.0.21", "172.18.0.22", "172.18.0.23"})
 	setupTest(config)
 	config.verifyRWReplicaCount(3)
-	config.runIOs()
+	go config.testSequentialData()
 	go config.snapshotCreateDelete()
 	ctrlClient := getControllerClient(config.ControllerIP)
 	startTime := time.Now()
@@ -60,18 +67,23 @@ func restartTwoReplicasTest() {
 		startContainer(config.Replicas[striped(replicas[0].Address)])
 		startContainer(config.Replicas[striped(replicas[1].Address)])
 		config.verifyRWReplicaCount(3)
-		time.Sleep(30 * time.Second)
-		if time.Since(startTime) > time.Minute*20 {
+		time.Sleep(65 * time.Second) // 65 so that auto-snapshot delete is triggered
+		if time.Since(startTime) > time.Minute*TestRunTime {
 			break
 		}
 		iteration++
 	}
 	config.Stop = true
-	for {
+	for range time.NewTicker(5 * time.Second).C {
 		if config.ThreadCount == 0 {
 			break
 		}
+		logrus.Infof(
+			"RestartControllerTest Completed, waiting for threads to exit, pending tc:%v",
+			config.ThreadCount,
+		)
 	}
+	logrus.Infof("Exiting RestartTwoReplicaTest, threads exited successfully")
 	scrap(config)
 }
 
@@ -79,7 +91,7 @@ func restartThreeReplicasTest() {
 	config := buildConfig("172.18.0.30", []string{"172.18.0.31", "172.18.0.32", "172.18.0.33"})
 	setupTest(config)
 	config.verifyRWReplicaCount(3)
-	config.runIOs()
+	go config.testSequentialData()
 	go config.snapshotCreateDelete()
 	ctrlClient := getControllerClient(config.ControllerIP)
 	startTime := time.Now()
@@ -97,18 +109,23 @@ func restartThreeReplicasTest() {
 		startContainer(config.Replicas[striped(replicas[1].Address)])
 		startContainer(config.Replicas[striped(replicas[2].Address)])
 		config.verifyRWReplicaCount(3)
-		time.Sleep(30 * time.Second)
-		if time.Since(startTime) > time.Minute*20 {
+		time.Sleep(65 * time.Second) // 65 so that auto-snapshot delete is triggered
+		if time.Since(startTime) > time.Minute*TestRunTime {
 			break
 		}
 		iteration++
 	}
 	config.Stop = true
-	for {
+	for range time.NewTicker(5 * time.Second).C {
 		if config.ThreadCount == 0 {
 			break
 		}
+		logrus.Infof(
+			"RestartControllerTest Completed, waiting for threads to exit, pending tc:%v",
+			config.ThreadCount,
+		)
 	}
+	logrus.Infof("Exiting RestartThreeReplicaTest, threads exited successfully")
 	scrap(config)
 }
 
@@ -116,7 +133,7 @@ func restartControllerTest() {
 	config := buildConfig("172.18.0.40", []string{"172.18.0.41", "172.18.0.42", "172.18.0.43"})
 	setupTest(config)
 	config.verifyRWReplicaCount(3)
-	config.runIOs()
+	go config.testSequentialData()
 	go config.snapshotCreateDelete()
 	startTime := time.Now()
 	iteration := 1
@@ -125,23 +142,30 @@ func restartControllerTest() {
 		stopContainer(config.Controller[striped(config.ControllerIP)])
 		startContainer(config.Controller[striped(config.ControllerIP)])
 		config.verifyRWReplicaCount(3)
-		time.Sleep(30 * time.Second)
-		if time.Since(startTime) > time.Minute*20 {
+		time.Sleep(65 * time.Second) // 65 so that auto-snapshot delete is triggered
+		if time.Since(startTime) > time.Minute*TestRunTime {
 			break
 		}
 		iteration++
 	}
 	config.Stop = true
-	for {
+	for range time.NewTicker(5 * time.Second).C {
 		if config.ThreadCount == 0 {
 			break
 		}
+		logrus.Infof(
+			"RestartControllerTest Completed, waiting for threads to exit, pending tc:%v",
+			config.ThreadCount,
+		)
 	}
+	logrus.Infof("Exiting RestartControllerTest, threads exited successfully")
 	scrap(config)
 }
 func chaosTest() {
+	logrus.Infof("Start chaos Test")
 	var wg sync.WaitGroup
 	wg.Add(4)
+
 	go func() {
 		restartOneReplicaTest()
 		wg.Done()
@@ -159,4 +183,5 @@ func chaosTest() {
 		wg.Done()
 	}()
 	wg.Wait()
+	logrus.Infof("Chaos Test completed successfully")
 }
